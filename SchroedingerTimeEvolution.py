85de8b0e35c2ae614b2321e0734f24abf971d48e
Aram Jamal <aramyamal@gmail.com>
2023-09-29 00:30:47 +0200

    added comments and example animations

--- SchroedingerTimeEvolution.py
+++ SchroedingerTimeEvolution.py
@@ -6,54 +6,56 @@ from scipy.constants import hbar
 
 class SchroedingerTimeEvolution:
     def __init__(self, Delta=1e-3, deltatau=1e-4, runtime = 300, sigma_0=0.04, sigma_0_pyr=6.439e-12, k_0=20, pyr_inv_distance = 1/8, x_0=38.15e-12, m = 2.47*1.66e-27):
-        self.Delta = Delta
-        self.deltatau = deltatau
-        self.T_k = int(runtime/self.deltatau * 1e-4)
-        self.N = int(1/Delta)
-        self.z_n = np.linspace(-1/2, 1/2, self.N)
-        self.sigma_0 = sigma_0
-        self.k_0 = k_0
-        self.pyr_inv_distance = pyr_inv_distance
+        self.Delta = Delta                              # position step size
+        self.deltatau = deltatau                        # time step size
+        self.T_k = int(runtime/self.deltatau * 1e-4)    # number of time steps   # runtime is the total time of the simulation in dimensionless units (tau)
+        self.N = int(1/Delta)                           # number of position steps
+        self.z_n = np.linspace(-1/2, 1/2, self.N)       # position array
+        self.sigma_0 = sigma_0                          
+        self.k_0 = k_0                                  
+        self.pyr_inv_distance = pyr_inv_distance        # the normalized distance between nitrogen atom and plane of hydrogen atoms during 
+                                                        # pyramidal inversion. Determines the width of the wave function and its position in
+                                                        # the infinite square well potential it is placed.
         self.sigma_0_pyr = sigma_0_pyr
-        self.x_0 = x_0
-        self.m = m
-        self.a = self.x_0/self.pyr_inv_distance
+        self.x_0 = x_0                                  # position of nitrogen atom during pyramidal inversion
+        self.m = m                                      # =3m_N * m_H / (3m_N + m_H) where m_N is the mass of nitrogen atom and m_H is the mass of hydrogen atom                            
+        self.a = self.x_0/self.pyr_inv_distance         # normalization factor for pyramidal inversion
 
     def psi_numerical_x(self, z):
+        """Wave function of a free particle in an infinite square well potential at t=0."""
         return np.exp((-z**2)/(4*self.sigma_0**2) + 1j*self.k_0*z)
     
     def psi_analytical_x_t(self, z, tau=0):
-        #does not have intital velocity, but origin state is correct
+        """Analytical solution to the time dependent Schroedinger equation for a free particle in an infinite square well potential."""
         b = np.sqrt(self.sigma_0**2 + 1j*tau/2)
         return 1/b * np.exp(-z**2/(4*b**2))
     
-    def psi_analytical_alt_x_t(self, z, tau=0):
-        #found online, corresponds to the numerical solution
+    def psi_analytical_alt_x_t(self, z, tau=0):         # found online, corresponds to the numerical solution
+        """
+        Alternate analytical solution to the time dependent Schroedinger equation for a free particle in an infinite square well potential.
+        This solution has initial velocity equal to that of psi_numerical_x, and is therefore more comparable to the numerical solution.
+        Found online at https://physicspages.com/pdf/Quantum%20mechanics/Free%20particle%20as%20moving%20Gaussian%20wave%20packet.pdf
+        """
         b = np.sqrt(1 + 1j*tau/(2*self.sigma_0**2))
         return 1/b * np.exp(-z**2/(4*self.sigma_0**2*b**2)) * np.exp((4*1j*self.sigma_0**2*self.k_0*z-1j*2*self.sigma_0**2*self.k_0**2*tau) / (4*self.sigma_0**2*b**2))
 
     def psi_pyramidal_inversion_x(self, z):
-        #intital wave function of nitrogen atom in ammonia molecule (NH3). Used to showcase pyramidal inversion
-        
+        """Unitless wave function of nitrogen atom in ammonia molecule (NH3) at time t=0. Used to show pyramidal inversion"""
         return np.exp(-(z-self.x_0/self.a)**2 / (4*(self.sigma_0_pyr/self.a)**2))
 
     def v_infsquare(self, z):
+        """The time independent potential energy for a free particle in an infinite square well potential."""
         if abs(z) > 1/2:
             return float("inf")
         else:
             return 0
     
     def v_pyramidal_inversion(self, z, omega_0=3.033e14):
-        #the potential energy for the pyramidal inversion of the nitrogen atom in the ammonia molecule (NH3)
+        """The time independent potential energy of a nitrogen atom in the ammonia molecule (NH3). Used to show pyramidal inversion."""
         return (self.a**4 * self.m**2 * omega_0**2) / (8*hbar**2 * (self.x_0/self.a)**2) * (z**2 - (self.x_0/self.a)**2)**2
 
-    def v_test(self, z):
-        if abs(z) > 1/2:
-            return 0
-        else:
-            return 10000*z
-        
     def tau_to_t(self, tau):
+        """Converts unitless time tau to seconds."""
         return self.a**2 * self.m * tau / hbar
         
     def Crank_Nicholson_matrix(self):
@@ -68,8 +70,11 @@ class SchroedingerTimeEvolution:
         return np.matmul(Crank_plus, Crank_minus)
 
     def numerical_schroedinger_time_evolution(self, psi_func=psi_numerical_x, v_func=v_infsquare, normalize_every_step=True):
+        """
+        Calculates the numerical time evolution of a wave function from tau=0 to tau=runtime.
+        Outputs a 2D array of shape (T_k, N) where T_k is the number of time steps and N is the number of position steps.
+        """
         print('calculating numerical time evolution: 0%', end='\r')
-        N = int(1/self.Delta)
         psi_k = np.array([psi_func(z) for z in self.z_n])
         psi_k[0] = 0
         psi_k[-1] = 0
@@ -77,10 +82,10 @@ class SchroedingerTimeEvolution:
         psi_k = psi_k * normalization_factor
         self.v_k = np.array([v_func(z) for z in self.z_n])
         M = self.Crank_Nicholson_matrix()
-        psi_k_t = np.zeros((self.T_k, N), dtype=complex)
+        psi_k_t = np.zeros((self.T_k, self.N), dtype=complex)
         psi_k_t[0] = psi_k
 
-        if normalize_every_step == True:
+        if normalize_every_step == True:        # in case wave function is not intially normalized
             for i in range(1, self.T_k):
                 psi_k_t[i] = np.matmul(M, psi_k_t[i-1])
                 normalization_factor = 1/np.sqrt(np.sum(abs(psi_k_t[i])**2)*self.Delta)
@@ -98,13 +103,16 @@ class SchroedingerTimeEvolution:
         return psi_k_t
 
     def analytical_schroedinger_time_evolution(self, psi_func=psi_analytical_alt_x_t):
+        """
+        Calculates the analytical time evolution of a wave function from tau=0 to tau=runtime, given that the wave function is function of time and position.
+        Outputs a 2D array of shape (T_k, N) where T_k is the number of time steps and N is the number of position steps.
+        """
         print('calculating analytical time evolution: 0%', end='\r')
         psi_k = np.array([psi_func(z) for z in self.z_n])
         normalization_factor = 1/np.sqrt(np.sum(abs(psi_k)**2)*self.Delta)
         psi_k = psi_k * normalization_factor
         psi_k_t = np.zeros((self.T_k, self.N), dtype=complex)
         psi_k_t[0] = psi_k
-        # print(f'calculating analytical time evolution: 0%', end='\r')
         for i in range(1, self.T_k):
             psi_k_t[i] = np.array([psi_func(z, tau=i*self.deltatau*2) for z in self.z_n]) * normalization_factor
             if i / self.T_k * 100 % 5 == 0:
@@ -113,7 +121,16 @@ class SchroedingerTimeEvolution:
         return psi_k_t
     
     def animate_distribution(self, psi_k_t_1, psi_k_t_2 = None, double_plot=False, save_gif=False, save_mp4=False, interval=30, plot_type='pdf', plot_every_nth_step=1):
-        
+        """
+        Plots the time evolution of one or two wave functions in position space as an animation.
+        plot_type='pdf' plots the probability density function of the wave function.
+        plot_type='real_vs_imaginary' plots the real and imaginary parts of the wave function.
+        plot_type='pdf_pyr_inv' plots the probability density function of the wave function during pyramidal inversion.
+        double_plot=True plots two wave functions in the same plot.
+        plot_every_nth_step=1 plots every step, plot_every_nth_step=2 plots every second step, etc.
+        interval=30 is the time between frames in milliseconds.
+        Saving as gif/mp4 file is saved in the same directory. FFmpeg is required to save as mp4.
+        """
         def save_gif_function(filename):
             current_dir = dirname(__file__)
             file_path = join(current_dir, filename + ".gif")
@@ -279,13 +296,13 @@ class SchroedingerTimeEvolution:
                 save_mp4_function(filename='pdf_pyr_inv')
             plt.show()
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    # S = SchroedingerTimeEvolution(Delta=1e-3, deltatau=1e-4, runtime=400)
-    # psi_k_t_analytical = S.analytical_schroedinger_time_evolution(psi_func=S.psi_analytical_alt_x_t)
-    # psi_k_t_numerical = S.numerical_schroedinger_time_evolution(psi_func=S.psi_numerical_x, v_func=S.v_infsquare)
-    # S.animate_distribution(psi_k_t_numerical, psi_k_t_analytical, double_plot=True, interval=30, save_gif=True, plot_type='pdf')
+#     S = SchroedingerTimeEvolution(Delta=1e-3, deltatau=1e-4, runtime=400)
+#     psi_k_t_analytical = S.analytical_schroedinger_time_evolution(psi_func=S.psi_analytical_alt_x_t)
+#     psi_k_t_numerical = S.numerical_schroedinger_time_evolution(psi_func=S.psi_numerical_x, v_func=S.v_infsquare)
+#     S.animate_distribution(psi_k_t_numerical, psi_k_t_analytical, double_plot=True, interval=30, save_gif=True, plot_type='pdf')
 
-    pyr_inv = SchroedingerTimeEvolution(Delta=1e-3, deltatau=1e-3, runtime=850000, pyr_inv_distance = 1/8)
-    psi_k_t_pyr_inv = pyr_inv.numerical_schroedinger_time_evolution(psi_func=pyr_inv.psi_pyramidal_inversion_x, v_func=pyr_inv.v_pyramidal_inversion, normalize_every_step=True)
-    pyr_inv.animate_distribution(psi_k_t_pyr_inv, interval=1, save_gif=True, save_mp4=True, plot_type='pdf_pyr_inv', plot_every_nth_step=60)
+#     pyr_inv = SchroedingerTimeEvolution(Delta=1e-2, deltatau=1e-3, runtime=850000, pyr_inv_distance = 1/8)
+#     psi_k_t_pyr_inv = pyr_inv.numerical_schroedinger_time_evolution(psi_func=pyr_inv.psi_pyramidal_inversion_x, v_func=pyr_inv.v_pyramidal_inversion, normalize_every_step=True)
+#     pyr_inv.animate_distribution(psi_k_t_pyr_inv, interval=1, save_gif=False, save_mp4=False, plot_type='pdf_pyr_inv', plot_every_nth_step=60)
